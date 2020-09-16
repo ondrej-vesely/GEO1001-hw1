67c85aec0bbeefb6bc01f9c84684ce27c0075134
Ondrej Vesely <ondrej-vesely@seznam.cz>
2020-09-16 14:57:00 +0200

    a1-1

--- _data.py
+++ _data.py
@@ -7,8 +7,8 @@ import numpy as np
 
 
 def parse(path):
-    """Parse heat stress sensor data excel sheets into dicts of value arrays."""
-    
+    """Parse heat stress sensor data excel sheet into dicts of value arrays."""
+
     raw_data = pd.read_excel(path)
     raw_data = np.array(raw_data)
     raw_data = np.swapaxes(raw_data, 0, 1)
@@ -24,11 +24,11 @@ def parse(path):
                 try:
                     values[i] = float(val)
                 except:
-                    pass
+                    values[i] = None
 
         data[measure] = {}
-        data[measure]['unit'] = units
-        data[measure]['vals'] = values
+        data[measure]['units'] = units
+        data[measure]['values'] = values
     
     return data
 
@@ -38,3 +38,11 @@ heat_b = parse('./data/HEAT - B_final.xls')
 heat_c = parse('./data/HEAT - C_final.xls')
 heat_d = parse('./data/HEAT - D_final.xls')
 heat_e = parse('./data/HEAT - E_final.xls')
+
+data = {
+    'Sensor A': heat_a,
+    'Sensor B': heat_b,
+    'Sensor C': heat_c,
+    'Sensor D': heat_d,
+    'Sensor E': heat_e
+    }
--- a1.py
+++ a1.py
@@ -1,3 +1,24 @@
 #-- GEO1001.2020--hw01
 #-- Ondrej Vesely
 #-- 5162130
+
+
+from _data import *
+
+
+def print_stats(data):
+    for sensor_name, sensor in data.items():
+        print('\n', sensor_name, '_'*70, '\n')
+    
+        for name, measure in sensor.items():
+            if name != 'FORMATTED DATE-TIME':
+                print("  %s [%s]:" % (name, measure['units']))
+                v = measure['values']
+                print("    mean = %s  var = %s  st.var. = %s" % (v.mean(), v.var(), v.std()))
+
+
+def main():
+    if 'y' in input("Print out mean statistics for all variables? y/n:  "):
+        print_stats(data)
+
+main()
