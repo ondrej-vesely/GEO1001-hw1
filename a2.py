3353e56f1fffd75fe596d4683ce69212d26f1be3
Ondrej Vesely <ondrej-vesely@seznam.cz>
2020-09-21 21:00:54 +0200

    a2-2

--- a2.py
+++ a2.py
@@ -5,6 +5,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
 from _data import data
 from _prompt import prompt
@@ -57,8 +58,36 @@ def show_distr(data, bins=50):
 
 
 # Question 2
-def show_kernel(data):
-    pass
+def show_kernel(data, bins=50):
+    try: bins = int(bins)
+    except: bins = 50
+
+    variable = 'Wind Speed'
+    fig = plt.figure(figsize=(18,4))
+
+    # PDF + KDE
+    for i, (sensor_name, sensor) in enumerate(data.items()):
+        ax = plt.subplot2grid((1,5), (0,i))
+        values = sensor[variable]['values']
+        
+        a = ax.hist(values, bins=bins, density=True, alpha=0.0)
+        hist_x, hist_y = (a[1][1:]-(a[1][1:]-a[1][:-1])/2, a[0])
+        
+        density = stats.gaussian_kde(list(values))
+        xs = np.linspace(0,8,200)
+
+        ax.plot(hist_x, hist_y, label='PDF')
+        ax.plot(xs, density(xs), label='KDE')
+        ax.set_ylim(0, 1)
+        ax.set_xlabel('%s [%s]' % (variable, sensor[variable]['units']))
+        ax.set_ylabel('Probability Density (%s bins)' % bins)
+        
+        ax.set_title(sensor_name)
+        ax.legend(loc='upper right')
+  
+    plt.tight_layout(pad=2.0)
+    plt.legend()
+    plt.show()
 
 
 
@@ -67,6 +96,9 @@ def main():
 
     if prompt("Show PMF, PDF and CDF distribution plots?"):
         show_distr(data, input('  Number of PDF bins:  '))
+    
+    if prompt("Show PDF and KDE plots?"):
+        show_kernel(data, input('  Number of PDF bins:  '))
 
 
 if __name__ == '__main__':
